e3dd4b970cdccc652b51b57e66bed8eb2a70490a
yunsubin <yunsubin481@gmail.com>
2024-08-04 10:25:42 +0900

    feature: dashboard charts [collectionsCount, threatCount, threatLog]

--- src/server/routers/dashboard.tsx
+++ src/server/routers/dashboard.tsx
@@ -31,16 +31,30 @@ const diskUsageBuffer: {
   free: number;
   usagePercentage: number;
 }[] = [];
+const collectionsCountBuffer: {
+  time: string;
+  total: number;
+}[] = [];
+const threatCountBuffer: {
+  device: string;
+  category: string;
+  amount: number;
+}[] = [];
+const threatLogBuffer: {
+  device: string;
+  category: string;
+  amount: number;
+}[] = [];
 
 function recordCpuUsage() {
-  const time = dayjs().format('HH:mm:ss');
+  const time = dayjs().format('HH:mm');
 
   const cpuUsages = os.cpus().map((cpu, index) => {
     const total = Object.values(cpu.times).reduce((acc, time) => acc + time, 0);
     const idle = cpu.times.idle;
     const usage = 100 - (idle / total) * 100;
     return {
-      [`core_${index + 1}`]: Number(usage.toFixed(2)),
+      [`core_${index + 1}`]: Number(usage.toFixed(3)),
     };
   });
 
@@ -52,7 +66,7 @@ function recordCpuUsage() {
   const cpuUsageEntry = {
     time,
     ...Object.assign({}, ...cpuUsages),
-    total_usage: Number((totalUsage / cpuUsages.length).toFixed(2)),
+    total_usage: Number((totalUsage / cpuUsages.length).toFixed(3)),
   };
 
   cpuUsageBuffer.push(cpuUsageEntry);
@@ -63,12 +77,12 @@ function recordCpuUsage() {
 }
 
 function recordMemoryUsage() {
-  const time = dayjs().format('HH:mm:ss');
+  const time = dayjs().format('HH:mm');
 
   const totalMemory = os.totalmem();
   const freeMemory = os.freemem();
   const usedMemory = totalMemory - freeMemory;
-  const usagePercentage = ((usedMemory / totalMemory) * 100).toFixed(2);
+  const usagePercentage = ((usedMemory / totalMemory) * 100).toFixed(3);
 
   const memoryUsageEntry = {
     time,
@@ -115,13 +129,13 @@ function recordDiskUsage() {
           return;
         }
 
-        const time = dayjs().format('HH:mm:ss');
+        const time = dayjs().format('HH:mm');
         const total = data[0];
         const used = data[5];
         const available = data[5];
 
         const usagePercentage = Number(
-          (((used ?? 0) / (total ?? 1)) * 100).toFixed(2)
+          (((used ?? 0) / (total ?? 1)) * 100).toFixed(3)
         );
 
         diskUsageBuffer.push({
@@ -131,7 +145,9 @@ function recordDiskUsage() {
           free: available ?? 0,
           usagePercentage,
         });
-
+        if (diskUsageBuffer.length > 600) {
+          diskUsageBuffer.shift();
+        }
         resolve();
       }
     );
@@ -152,11 +168,47 @@ function recordCollectionsCount() {
         })
         .then((data) => {
           totalCnt = data.data.rows[0][0];
-
-          console.log('totalCnt : ' + totalCnt);
+          collectionsCountBuffer.push({
+            time: dayjs().format('HH:mm'),
+            total: totalCnt,
+          });
+          if (collectionsCountBuffer.length > 600) {
+            collectionsCountBuffer.shift();
+          }
         });
     } catch {
-      console.log('error');
+      reject('Record Collections Count error');
+    }
+    resolve();
+  });
+}
+
+function recordThreatCount() {
+  return new Promise<void>(async (resolve, reject) => {
+    const queryThreatCnt = `SELECT DEVICE, THR_CATEGORY, COUNT(THR_CATEGORY) AMOUNT AMOUNT FROM PANETLOG WHERE TYPE = 'THREAT'`;
+    try {
+      await fetch(
+        `${env.MACHBASE_URL}:${env.MACHBASE_PORT}/db/query?q=` +
+          encodeURIComponent(queryThreatCnt)
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          const logs = data.data;
+          for (let i = 0; i < logs.rows.length; i++) {
+            threatCountBuffer.push({
+              device: logs.rows[i][0] as string,
+              category: logs.rows[i][1] as string,
+              amount: logs.rows[i][2] as number,
+            });
+          }
+          if (threatCountBuffer.length > 600) {
+            threatCountBuffer.shift();
+          }
+        });
+    } catch {
+      reject('Record Threat Count error');
     }
     resolve();
   });
@@ -173,6 +225,8 @@ function startRecording() {
     await recordCpuUsage();
     await recordMemoryUsage();
     await recordDiskUsage();
+    await recordCollectionsCount();
+    await recordThreatCount();
   }, 1000);
 }
 
@@ -208,7 +262,7 @@ export const dashboardRouter = createTRPCRouter({
       return (
         cpuUsageBuffer ?? [
           {
-            time: dayjs().format('HH:mm:ss'),
+            time: dayjs().format('HH:mm'),
             core_1: 0,
             core_2: 0,
             core_3: 0,
@@ -292,13 +346,104 @@ export const dashboardRouter = createTRPCRouter({
         ]
       );
     }),
+  getCollectionsCounts: protectedProcedure()
+    .meta({
+      openapi: {
+        method: 'GET',
+        path: '/dashboard/collections-counts',
+        protect: true,
+        tags: ['dashboard'],
+      },
+    })
+    .input(z.object({}))
+    .output(
+      z.array(
+        z.object({
+          time: z.string(),
+          total: z.number(),
+        })
+      )
+    )
+    .query(async ({ ctx }) => {
+      ctx.logger.info('Getting disk usage');
+      return (
+        collectionsCountBuffer || [
+          {
+            time: '',
+            total: 0,
+          },
+        ]
+      );
+    }),
+  getThreatLogData: protectedProcedure()
+    .meta({
+      openapi: {
+        method: 'GET',
+        path: '/dashboard/threat-counts',
+        protect: true,
+        tags: ['dashboard'],
+      },
+    })
+    .input(z.object({}))
+    .output(
+      z.array(
+        z.object({
+          device: z.string(),
+          category: z.string(),
+          amount: z.number(),
+        })
+      )
+    )
+    .query(async ({ ctx }) => {
+      ctx.logger.info('Getting disk usage');
+      return (
+        threatCountBuffer || [
+          {
+            device: '',
+            category: '',
+            amount: 0,
+          },
+        ]
+      );
+    }),
+  getSystemLog: protectedProcedure()
+    .meta({
+      openapi: {
+        method: 'GET',
+        path: '/dashboard/system-log',
+        protect: true,
+        tags: ['dashboard'],
+      },
+    })
+    .input(z.object({}))
+    .output(
+      z.array(
+        z.object({
+          device: z.string(),
+          category: z.string(),
+          amount: z.number(),
+        })
+      )
+    )
+    .query(async ({ ctx }) => {
+      ctx.logger.info('Getting system log');
+      return (
+        threatLogBuffer || [
+          {
+            device: '',
+            category: '',
+            amount: 0,
+          },
+        ]
+      );
+    }),
 });
 
 export async function GET() {
   const totalMemory = os.totalmem();
   const freeMemory = os.freemem();
   const usedMemory = totalMemory - freeMemory;
-  const usagePercentage = ((usedMemory / totalMemory) * 100).toFixed(2);
+  const usagePercentage = ((usedMemory / totalMemory) * 100).toFixed(3);
 
   const response = {
     total: totalMemory,
